58ccf37dcd50de22d92d6cfbb48fa73c4aba2d97
Kristjan Kosic <kristjan@protokol.com>
2020-11-17 10:57:49 +0100

    chore: make publish script npm based

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd packages
-cd guardian-crypto && yarn publish:beta && cd ..
-cd guardian-transactions && yarn publish:beta && cd ..
-cd guardian-api && yarn publish:beta && cd ..
+cd guardian-crypto && npm publish --tag beta --access public --tolerate-republish && cd ..
+cd guardian-transactions && npm publish --tag beta --access public --tolerate-republish && cd ..
+cd guardian-api && npm publish --tag beta --access public --tolerate-republish && cd ..
