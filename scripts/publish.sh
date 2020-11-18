b7fd859f97d070551586ed9d163b124e93aed140
Kristjan Kosic <kristjan@protokol.com>
2020-11-18 09:36:57 +0100

    ci: remove old script

--- scripts/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd packages
-cd guardian-crypto && npm publish --tag beta --access public --tolerate-republish && cd ..
-cd guardian-transactions && npm publish --tag beta --access public --tolerate-republish && cd ..
-cd guardian-api && npm publish --tag beta --access public --tolerate-republish && cd ..
